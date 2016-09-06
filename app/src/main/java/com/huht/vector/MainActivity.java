df06a4ffbe011a7fa1ed09b1b7355675a8bbb56f
huht <286860898@qq.com>
2016-09-06 16:40:43 +0800

    selector

--- app/src/main/java/com/huht/vector/MainActivity.java
+++ app/src/main/java/com/huht/vector/MainActivity.java
@@ -1,9 +1,14 @@
 package com.huht.vector;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
